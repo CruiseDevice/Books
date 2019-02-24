0244ace63c331a199e4e6d48a366729c4fc3341b
CruiseDevice <meakashchavan@gmail.com>
2019-02-24 10:38:56 +0530

    Code refactoring - Delete unnecessary newlines in _config.py - Use db.init_app() and db.create_all() functions in views.py to migrate   datebase on run server. - update requirements.txt

--- _config.py
+++ _config.py
@@ -3,13 +3,9 @@ import credentials
 
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
-
 DATABASE = credentials.DATABASE
-
 WTF_CSRF_ENABLED = True
-
 SECRET_KEY = credentials.SECRET_KEY
-
 DATABASE_PATH = os.path.join(BASEDIR, DATABASE)
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///'+DATABASE_PATH
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(DATABASE_PATH)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from views import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class User(db.Model):
 	__tablename__ = 'users'
