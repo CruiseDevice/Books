e21b058bfb667c1a497263fe9078238630204229
CruiseDevice <meakashchavan@gmail.com>
2019-02-24 15:49:32 +0530

    Add User registration

--- forms.py
+++ forms.py
@@ -1,7 +1,38 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField
+from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class LoginForm(Form):
-	name = StringField('Username', validators=[DataRequired()])
+	name = TextField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
+
+class SignUpForm(Form):
+	email = TextField(
+					'Email',
+					validators=[
+								DataRequired(),
+								Email(),
+								Length(min=6, max=25)
+							   ]
+					)
+	username = TextField(
+						'Username',
+						validators=[
+									DataRequired(),
+									Length(min=6, max=25)
+									]
+						)
+	password = PasswordField(
+							'Password',
+							validators=[
+										DataRequired(),
+										Length(min=6, max=40)
+										]
+							)
+	confirm_password = PasswordField(
+									'Repeat Password',
+									validators=[
+												DataRequired(),
+												EqualTo('password')
+												]
+									)
--- views.py
+++ views.py
@@ -2,12 +2,19 @@ import os
 import sqlite3
 import requests
 
-from flask import Flask, session, flash, request, redirect, render_template, url_for
+from flask import (
+		  Flask, session, flash, request, redirect, render_template,
+		  url_for
+)
 from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.exc import IntegrityError
+
+from werkzeug.security import generate_password_hash
 
 from credentials import API_KEY, API_SECRET
 
-from forms import LoginForm
+from forms import LoginForm, SignUpForm
 from models import User, db
 
 app = Flask(__name__)
@@ -20,7 +27,8 @@ def connect_db():
 
 @app.route('/index/')
 def index():
-	res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key":API_KEY, "isbns": "9781632168146"})
+	res = requests.get("https://www.goodreads.com/book/review_counts.json",
+			params={"key":API_KEY, "isbns": "9781632168146"})
 	return render_template('index.html')
 
 @app.route("/", methods=['GET', 'POST'])
@@ -39,6 +47,30 @@ def login():
 		else:
 			error = 'Invalid username or password.'
 	return render_template('login.html',form=form, error=error)
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+	error = None
+	form  = SignUpForm(request.form)
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			username = form.username.data
+			email = form.email.data
+			password = form.password.data
+			password = generate_password_hash(password)
+			new_user = User(name=username, email=email, password=password)
+			try:
+				db.session.add(new_user)
+				db.session.commit()
+				flash('Thanks for registering. Please login.')
+				return redirect(url_for('login'))
+			except IntegrityError:
+				error = 'Sorry that username and/or email already exists.'
+				return render_template('regieter.html', form=form, error=error)
+		else:
+			return render_template('register.html', form=form, error=error)
+	if request.method == 'GET':
+		return render_template('register.html', form=form)
 
 @app.route('/logout')
 def logout():
