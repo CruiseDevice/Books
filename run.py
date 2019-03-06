158838a79f48a1db19c5f00319008b8bf30cfd37
CruiseDevice <meakashchavan@gmail.com>
2019-03-06 11:07:49 +0530

    Flask_login config in run.py

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
-from views import app
+from views import app, login_manager
+from models import AnonymousUser
+from _config import SECRET_KEY
+
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.session_protection = "strong"
+login_manager.anonymous_user = AnonymousUser
+
+@login_manager.user_loader
+def load_user(user_id):
+	return None
+
 app.run(debug=True)
